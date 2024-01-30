0fde3518c59fcb81175e3115e07fac2dd039235e
GabrielPuddu2106 <142625919+GabrielPuddu2106@users.noreply.github.com>
2024-01-30 14:51:49 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿{
+    Console.Title = "Sexo del individuo";
+
+    Int32 edad;
+
+    String name, sexo;
+
+    Console.WriteLine("Ingrese su name");
+    name = Console.ReadLine();  
+
+    Console.WriteLine("Ingrese la edad");
+    edad = Int32.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese su sexo (Hombre=M y Mujer=F)");
+    sexo = Console.ReadLine();  
+
+    if(sexo == "M"|| sexo == "m") // logica
+    {
+        Console.WriteLine("\nEl Sr {0} ha sido registrado", name); // v
+    }
+    else
+    {
+        Console.WriteLine("nLa Sra {0} ha sido registrada", name); // f
+    }
+
+    Console.WriteLine("\n\n");
+    Console.WriteLine("\n---->Fin del programa");
+    Console.ReadKey();
+}
