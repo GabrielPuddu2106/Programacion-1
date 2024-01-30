4d816b083e8b573e48c79fe357560d58076251b1
GabrielPuddu2106 <142625919+GabrielPuddu2106@users.noreply.github.com>
2024-01-30 15:54:35 -0600

    Add files via upload

---
+++ Fecha.cs
@@ -0,0 +1,74 @@
+﻿{
+    Console.Title = " La Fecha de una persona";
+
+    Int32 Ndia;
+    Int32 Nmes;
+    Int32 Dda = 0;
+
+    Console.WriteLine("Ingrese el numero de dia");
+    Ndia = Int32.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese el numero de mes");
+    Nmes = Int32.Parse(Console.ReadLine());
+
+    switch (Nmes)
+    {
+        case 1:
+            Dda = Ndia;
+            break;
+
+        case 2:
+            Dda = Ndia +31;
+            break;
+
+        case 3:
+            Dda = Ndia + 59;
+
+            break;
+
+        case 4:
+            Dda = Ndia + 90;
+            break;
+
+        case 5:
+            Dda = Ndia + 120;
+            break;
+
+        case 6:
+            Dda = Ndia + 151;
+            break;
+
+        case 7:
+            Dda = Ndia + 181;
+
+            break;
+
+        case 8:
+            Dda = Ndia + 212;
+            break;
+
+        case 9:
+            Dda = Ndia + 243;
+            break;
+
+        case 10:
+            Dda = Ndia + 273;
+            break;
+
+        case 11:
+            Dda = Ndia + 304;
+            break;
+
+        case 12:
+            Dda = Ndia + 334;
+            break;
+
+        default: Console.WriteLine("Datos incorrectos");
+
+            
+            return;
+    
+    }
+
+    Console.WriteLine("El dia " + Ndia + "Corresponde al numero de dia " + Dda);
+}
