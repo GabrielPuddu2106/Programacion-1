5e71340f2980f6abab2b69a322c2a1502aa1c057
GabrielPuddu2106 <142625919+GabrielPuddu2106@users.noreply.github.com>
2024-01-31 15:29:38 -0600

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,16 @@
+﻿{
+    Console.Title = " Cambio Dolar $ ";
+
+    Int32 dolar;
+
+    Double cambioeuro;
+
+    Console.WriteLine("Ingrese la cantidad de dolares que desea cambiar");
+    dolar = Int32.Parse(Console.ReadLine());
+
+    cambioeuro = (dolar * 1.33250);
+
+    // Conversion 
+
+    Console.WriteLine("Su cambio de moneda a euro es :" + cambioeuro);
+}
