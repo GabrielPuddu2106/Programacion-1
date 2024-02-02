ce93e13ef50e0d9b2918067ec94d17468c32f53e
GabrielPuddu2106 <142625919+GabrielPuddu2106@users.noreply.github.com>
2024-02-02 15:26:31 -0600

    Add files via upload

---
+++ ProgramMatrix.cs
@@ -0,0 +1,33 @@
+﻿static void Main(string[] args)
+{
+    Console.Title = "Ejemplol de una matriz 3x4";
+
+    int[,] Matrix;
+    Matrix = new int[3, 4];
+    //Cargar y visualizar una matriz
+    for (int filas = 0; filas < 3; filas++)
+    {// for externo para filas
+        for (int columnas = 0; columnas < 4; columnas++)
+        {// for interno para columnas
+            Console.Write("Ingrese el valor de la posición [" + filas + +columnas + "]: ");
+            Matrix[filas, columnas] = int.Parse(Console.ReadLine());
+
+            Console.Write("\n");
+            Console.Write("Ahora visualizamos la matrix digitada....");
+            Imprimir(Matrix);
+            Console.ReadKey();
+
+            static void Imprimir(int[,] Matrix)
+            {
+                for (int filas = 0; filas < 3; filas++)
+                {
+                    Console.Write("\n");
+                    for (int columnas = 0; columnas < 4; columnas++)
+                    {
+                        Console.Write("\t" + Matrix[filas, columnas] + "\t");
+                    }
+                }
+            }
+        }
+    }
+}
