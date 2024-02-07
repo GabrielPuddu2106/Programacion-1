4d38bb782024a49b303c0439531dd50452bfe0d2
GabrielPuddu2106 <142625919+GabrielPuddu2106@users.noreply.github.com>
2024-02-07 16:19:56 -0600

    Add files via upload

---
+++ Ccine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+   public  class CCine
+    {
+        public string nombre;
+        public string telefono;
+        public string pagweb;
+        public string direccionFacebook;
+
+        public CCine(string nombre, string telefono, string pagweb, string direccionFacebook)
+        {
+            this.nombre = nombre;
+            this.telefono = telefono;
+            this.pagweb = pagweb;
+            this.direccionFacebook = direccionFacebook;
+        }
+
+        public void mostrarDatos()
+        {
+            Console.WriteLine("El nombre es :" + nombre);
+            Console.WriteLine("El telefono es :" + telefono);
+            Console.WriteLine("El pagweb es: " + pagweb);
+            Console.WriteLine("La direccion es: " + direccionFacebook);
+        }
+
+    }
+
