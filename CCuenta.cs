b2dcdbdc62ec1633879a8942279768d6ba0909bd
GabrielPuddu2106 <142625919+GabrielPuddu2106@users.noreply.github.com>
2024-02-09 15:26:01 -0600

    Add files via upload

---
+++ CCuenta.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class CCuenta
+    {
+        //Atributos 
+
+        private string nombre;
+        private string cuenta;
+        private double saldo;
+        private double tipoDeInterés;
+
+
+        // Propiedades y métodos
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                if (value.Length == 0)
+                {
+                    System.Console.WriteLine("Error: cadena vacía");
+                    return;
+                }
+                nombre = value;
+            }
+        }
+        public string Cuenta
+        {
+
+            get
+            {
+                return cuenta;
+            }
+            set
+            {
+                if (value.Length == 0)
+                {
+                    System.Console.WriteLine("Error: cuenta no válida");
+                    return;
+                }
+                cuenta = value;
+            }
+        }
+
+        public double Saldo
+        {
+            get
+            {
+                return saldo;
+            }
+        }
+        public void ingreso(double cantidad)
+        {
+            if (cantidad < 0)
+            {
+                System.Console.WriteLine("Error: cantidad negativa");
+                return;
+            }
+            saldo = saldo + cantidad;
+        }
+
+
+
+        public void reintegro(double cantidad)
+        {
+            if (saldo - cantidad < 0)
+            {
+                System.Console.WriteLine("Error: no dispone de saldo");
+                return;
+            }
+            saldo = saldo - cantidad;
+        }
+
+
+        public double TipoDeInterés
+        {
+            get
+            {
+                return tipoDeInterés;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    System.Console.WriteLine("Error: tipo no válido");
+                    return;
+                }
+                tipoDeInterés = value;
+            }
+        }
+    }
+
+}
